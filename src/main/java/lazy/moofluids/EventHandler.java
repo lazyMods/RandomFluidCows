5ac28b5226526375b8dd6a1a4b24d599276cd104
lazynessmind <lazynessmind@gmail.com>
2021-06-01 23:20:32 +0100

    Fix: Wrong spawn rates; Spawning on wrong biomes.

--- src/main/java/lazy/moofluids/EventHandler.java
+++ src/main/java/lazy/moofluids/EventHandler.java
@@ -18,19 +18,22 @@ public class EventHandler {
 
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public static void onBiomeLoading(BiomeLoadingEvent event) {
-        if (event.getCategory() == Biome.Category.NETHER
+        if(event.getCategory() == Biome.Category.NETHER
                 || event.getCategory() == Biome.Category.OCEAN
+                || event.getCategory() == Biome.Category.BEACH
+                || event.getCategory() == Biome.Category.DESERT
                 || event.getCategory() == Biome.Category.RIVER
                 || event.getCategory() == Biome.Category.THEEND) return;
         List<MobSpawnInfo.Spawners> spawners = event.getSpawns().getSpawner(EntityClassification.CREATURE);
-        spawners.add(new MobSpawnInfo.Spawners(Setup.MOO_FLUID.get(), 8, 4, 4));
+        spawners.add(new MobSpawnInfo.Spawners(Setup.MOO_FLUID.get(), Configs.SPAWN_WEIGHT.get(), Configs.SPAWN_MIN_COUNT.get(), Configs.SPAWN_MAX_COUNT.get()));
     }
 
     @SubscribeEvent
-    public static void onEntityJoinWorld(EntityJoinWorldEvent event){
-        if(event.getEntity() instanceof MooFluidEntity){
+    public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        if(event.getWorld().isClientSide) return;
+        if(event.getEntity() instanceof MooFluidEntity) {
             MooFluidEntity mooFluidEntity = (MooFluidEntity) event.getEntity();
-            if(mooFluidEntity.getFluid() == null || mooFluidEntity.getFluid() == Fluids.EMPTY){
+            if(mooFluidEntity.getFluid() == null || mooFluidEntity.getFluid() == Fluids.EMPTY) {
                 mooFluidEntity.setHealth(0);
             }
         }
