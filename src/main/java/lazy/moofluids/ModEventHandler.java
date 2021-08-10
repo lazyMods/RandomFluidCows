5bb0e502f250a21b6807f18057091d1e765bc0f0
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-08-10 10:36:29 +0100

    Fix server crash

--- src/main/java/lazy/moofluids/ModEventHandler.java
+++ src/main/java/lazy/moofluids/ModEventHandler.java
@@ -1,11 +1,12 @@
 package lazy.moofluids;
 
 import lazy.moofluids.utils.FluidColorFromTexture;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MooFluids.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = MooFluids.MOD_ID, value = Dist.CLIENT)
 public class ModEventHandler {
 
     public static boolean donePopulation = false;
